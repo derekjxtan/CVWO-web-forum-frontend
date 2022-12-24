910e78144da29d704dc88b2a8c3afb18bb8ca2a6
Derek Tan <derek.tan.jing.xu@gmail.com>
2022-12-24 12:35:43 +0800

    Added sorting functionality for posts

--- src/components/posts.js
+++ src/components/posts.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Card  from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
+import Col  from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 import { Link } from "react-router-dom";
 
@@ -14,14 +15,49 @@ import { fetchPosts } from "../reducers/postsSlice";
 export const Posts = () => {
     const dispatch = useDispatch()
 
+    const [order, setOrder] = useState(0);
+
     // find some way to handle missing dependency warning in eslint
     useEffect(() => {
         dispatch(fetchPosts());
     }, [])
 
-    const posts = useSelector(state => state.posts).posts
+    var posts = useSelector(state => state.posts).posts
+    
+    const sortLatest = () => {
+        if (order !== 0) {
+            setOrder(0);
+            sortPosts();
+        } 
+    }
 
-    const postsList = posts.map(post => (
+    const sortEarliest = () => {
+        if (order !== 1) {
+            setOrder(1);
+            sortPosts();
+        } 
+    }
+
+    // sorts posts array in selected order
+    const sortPosts = () => {
+        const postCopy = JSON.parse(JSON.stringify(posts))
+        // Latest to Earliest by date
+        if (order === 0) {
+            postCopy.sort((a, b) => {
+                return Date.parse(b.created_at) - Date.parse(a.created_at);
+            });
+        }
+        // Earliest to Latest by date
+        else {
+            postCopy.sort((a, b) => {
+                return Date.parse(a.created_at) - Date.parse(b.created_at);
+            });
+        }
+        return postCopy;
+    }
+
+    // const postsList = posts.map(post => (
+    const postsList = sortPosts().map(post => (
         <Card className="mt-3" key={post.id}>
             <Card.Header>
                 <Card.Title className="d-flex justify-content-start">{post.title}</Card.Title>
@@ -51,96 +87,23 @@ export const Posts = () => {
     ))
 
     return (
-        <Col xs={8} className='d-inline-flex justify-content-center'>
-            <Row lg={1} className='d-inline-flex justify-content-center align-items-center'>
-                {postsList}
+        // <Col xs={8} className='d-inline-flex justify-content-center'>
+        //     <Row lg={1} className='d-inline-flex justify-content-center align-items-center'>
+        //         <Button variant="primary" onClick={sortDateAsc}>Date ASC</Button>
+        //         <Button variant="primary" onClick={sortDateDsc}>Date DSC</Button>
+        //         {postsList}
+        //     </Row>
+        // </Col>
+        <Container className="col-8 mt-3">
+            <Row>
+                <Container>
+                    <Button variant="primary" className="float-end" onClick={sortEarliest}>Earliest</Button>
+                    <Button variant="primary" className="float-end me-1" onClick={sortLatest}>Latest</Button>
+                </Container>
             </Row>
-        </Col>
+            <Container>
+                {postsList}
+            </Container>
+        </Container>
     ) 
 }
-
-
-// class Posts extends React.Component {
-//     render() {
-//         // const posts = useSelector(state => state.posts)
-
-//         // const postsList = posts.map(post => (
-//         //     <Card className="mt-3" key={post.id}>
-//         //         <Card.Header>
-//         //             <Card.Title className="d-flex justify-content-start">{post.title}</Card.Title>
-//         //             <Row>
-//         //                 <Col className="d-flex justify-content-start">
-//         //                     <Card.Subtitle>@{post.username}, {post.created_at}</Card.Subtitle>
-//         //                 </Col>
-//         //                 <Col className="d-flex justify-content-end">
-//         //                     <Card.Subtitle>Likes: 5, Dislikes: 10</Card.Subtitle>
-//         //                 </Col>
-//         //             </Row>
-//         //             <Card.Subtitle className="d-flex justify-content-start mt-1">Tags: Fun, Games</Card.Subtitle>
-//         //         </Card.Header>
-//         //         <Card.Body>
-//         //             <Card.Text>{post.body.substring(0, 100)}</Card.Text>
-//         //             <div className="d-flex justify-content-end">
-//         //                 <Button variant='success' className="me-2">Like</Button>
-//         //                 <Button variant='danger' className="me-2">Dislike</Button>
-//         //                 <Nav.Link href="#placeholder" className="me-2"><Button variant="primary">See full</Button></Nav.Link>
-//         //                 <Button variant='secondary'>Save</Button>
-//         //             </div>
-//         //         </Card.Body>
-//         //     </Card>
-//         // ))
-
-//         return (
-//             <Col xs={8} className='d-inline-flex justify-content-center'>
-//                 <Row lg={1} className='d-inline-flex justify-content-center align-items-center'>
-//                     {/* {postsList} */}
-//                     <Card className="mt-3">
-//                         <Card.Header>
-//                             <Card.Title className="d-flex justify-content-start">Post 1 title</Card.Title>
-//                             <Row>
-//                                 <Col className="d-flex justify-content-start">
-//                                     <Card.Subtitle>@user1, 13 Dec 21 7.36pm</Card.Subtitle>
-//                                 </Col>
-//                                 <Col className="d-flex justify-content-end">
-//                                     <Card.Subtitle>Likes: 5, Dislikes: 10</Card.Subtitle>
-//                                 </Col>
-//                             </Row>
-//                             <Card.Subtitle className="d-flex justify-content-start mt-1">Tags: Fun, Games</Card.Subtitle>
-//                         </Card.Header>
-//                         <Card.Body>
-//                             <Card.Text>Post 1 body text is all here maybe it should be abit longer but idk what to put in for this so ill end it here</Card.Text>
-//                             <div className="d-flex justify-content-end">
-//                                 <Button variant='success' className="me-2">Like</Button>
-//                                 <Button variant='danger' className="me-2">Dislike</Button>
-//                                 <Nav.Link href="#placeholder" className="me-2"><Button variant="primary">See full</Button></Nav.Link>
-//                                 <Button variant='secondary'>Save</Button>
-//                             </div>
-//                         </Card.Body>
-//                     </Card>
-//                     <Card className="mt-3">
-//                         <Card.Header>
-//                             <Card.Title className="d-flex justify-content-start">Post 2 title</Card.Title>
-//                             <Row>
-//                                 <Col className="d-flex justify-content-start">
-//                                     <Card.Subtitle>@user1, 13 Dec 21 7.36pm</Card.Subtitle>
-//                                 </Col>
-//                                 <Col className="d-flex justify-content-end">
-//                                     <Card.Subtitle>Likes: 5, Dislikes: 10</Card.Subtitle>
-//                                 </Col>
-//                             </Row>
-//                         </Card.Header>
-//                         <Card.Body>
-//                             <Card.Text>Post 1 body text is all here maybe it should be abit longdshshjsdgkhsdlghdfgshldhgdsfkldsjdsghsdjkhdsflghjsdfhgkldsklhgljdgger but idk what to put in for this so ill end it here</Card.Text>
-//                             <div className="d-flex justify-content-end">
-//                                 <Button variant='success' className="me-2">Like</Button>
-//                                 <Button variant='danger'>Dislike</Button>
-//                             </div>
-//                         </Card.Body>
-//                     </Card>
-//                 </Row>
-//             </Col>
-//         );
-//     }
-// }
-
-// export default Posts;
